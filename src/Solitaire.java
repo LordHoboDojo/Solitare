95574c72735643a96cd4c8f9a2d234d6251041d1
LordHoboDojo <32212717+LordHoboDojo@users.noreply.github.com>
2019-11-09 20:08:40 -0600

    Final Version

--- src/Solitaire.java
+++ src/Solitaire.java
@@ -1,36 +1,63 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
-import java.util.logging.FileHandler;
 
 public class Solitaire {
     public static  void main (String[] args) throws FileNotFoundException {
         ArrayDeque<Card> deck;
-        LinkedList<ArrayDeque<Card>> piles = new LinkedList<>();
+        ArrayList<ArrayDeque<Card>> piles = new ArrayList<>();
         Scanner sc = new Scanner(new File("squeezebox.dat"));
-        while (sc.hasNextLine())
-        {
-             deck = getData(sc);
-             if (!deck.isEmpty()){
-                 while (!deck.isEmpty()){
-                     Card card = deck.removeFirst();
-                     ArrayList<String> moves  =new ArrayList<>();
-                     ArrayDeque<Card> pile = new ArrayDeque<>();
-                     pile.addFirst(card);
-                     piles.add(pile);
-                     for (ListIterator i = piles.listIterator();i.hasNext();)
-                     {
-                         if (i.hasPrevious())
-                         {
-                             
-                         }
-                     }
-                 }
-             }
+        while (sc.hasNextLine()) {
+            deck = getData(sc);
+            if (deck.isEmpty()) break;
+            while (!deck.isEmpty()) {
+                Card card = deck.removeFirst();
+                ArrayDeque<Card> pile = new ArrayDeque<>();
+                pile.addFirst(card);
+                piles.add(pile);
+            }
+            boolean one;
+            boolean three;
+            for (int i = 0; i < piles.size(); i++) {
+                one = false;
+                three = false;
+                if (piles.get(i).isEmpty()) {
+                    piles.remove(i);
+                    i--;
+                }
+                if (i >= 3) {
+                    if (piles.get(i).getFirst().match(piles.get(i - 3).getFirst())) three = true;
+                }
+                if (i >= 1) {
+                    if (piles.get(i).getFirst().match(piles.get(i - 1).getFirst())) one = true;
+                }
+                if (three) {
+                    ArrayDeque<Card> pileToMoveFrom = piles.get(i);
+                    ArrayDeque<Card> receivingPile = piles.get(i - 3);
+                    receivingPile.addFirst(pileToMoveFrom.removeFirst());
+                    piles.set(i, pileToMoveFrom);
+                    piles.set(i - 3, receivingPile);
+                    if (pileToMoveFrom.isEmpty()) piles.remove(i);
+                    i = -1;
+                } else if (one) {
+                    ArrayDeque<Card> pileToMoveFrom = piles.get(i);
+                    ArrayDeque<Card> receivingPile = piles.get(i - 1);
+                    receivingPile.addFirst(pileToMoveFrom.removeFirst());
+                    piles.set(i, pileToMoveFrom);
+                    piles.set(i - 1, receivingPile);
+                    if (pileToMoveFrom.isEmpty()) piles.remove(i);
+                    i = -1;
+                }
+            }
+            System.out.print(piles.size() + " piles remaining: ");
+            for (ArrayDeque<Card> pile:piles){
+                System.out.print(pile.size()+ " ");
+            }
+            System.out.println();
+            piles.clear();
         }
-    }
-
-    private static ArrayDeque<Card> getData(Scanner sc) {
+            }
+        private static ArrayDeque<Card> getData(Scanner sc) {
         ArrayDeque<Card> deck = new ArrayDeque<>();
         String lineOne = sc.nextLine();
         String lineTwo;
@@ -46,8 +73,6 @@ public class Solitaire {
         }
         return deck;
     }
-
-
 }
 class Card{
     public String getVal() {
@@ -65,10 +90,7 @@ class Card{
 
     @Override
     public String toString() {
-        return "Card{" +
-                "val=" + val +
-                ", suit='" + suit + '\'' +
-                '}';
+        return val+suit;
     }
     public boolean match(Card c){
         return c.getSuit().equals(this.getSuit()) || c.getVal().equals(this.getVal());
